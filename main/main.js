4efe5b61872c9de4bd39b27428af11e7933ca89e
jackhally <qq1223386926@gmail.com>
2019-05-30 14:04:37 +0800

    form a new function to calculating the price

--- main/main.js
+++ main/main.js
@@ -1,7 +1,10 @@
+function calcPrice(inputs) {
+  if(inputs["parkTime"] === 0) {
+    return 6;
+  }
+  return 7;
+}
 module.exports = function main(inputs) {
     // write your code here...
-    if(inputs["parkTime"] === 0) {
-      return 6;
-    }
-    return 7;
+    return calcPrice(inputs);
 };
